064cc92a5ad065be46c4d97acc774d230bab482d
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-08-05 21:50:19 -0400

    Typo

--- ambf6dpose/__init__.py
+++ ambf6dpose/__init__.py
@@ -1,7 +1,7 @@
 # WARNING: Swapping the order of the imports will trigger a circular import error.
 # Importing SimulationInterface first than the RosClients classes triggers the error.
 # https://medium.com/brexeng/avoiding-circular-imports-in-python-7c35ec8145ed
-# Todo: This requires a fix
+# TODO: This requires a fix
 
 from ambf6dpose.DataCollection.RosClients import (
     AbstractSimulationClient,
