95335bf7a6ba8cde4f72ca657bc8d9c910fab60a
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-08-17 01:33:11 -0400

    Clean ros clients

--- ambf6dpose/DataCollection/RosClients.py
+++ ambf6dpose/DataCollection/RosClients.py
@@ -1,21 +1,11 @@
 from __future__ import annotations
 import time
-from surgical_robotics_challenge.simulation_manager import SimulationManager
-from surgical_robotics_challenge.ecm_arm import ECM
-from surgical_robotics_challenge.scene import Scene
-from surgical_robotics_challenge.camera import Camera
 import numpy as np
-import tf_conversions.posemath as pm
-from sensor_msgs.msg import Image 
-from cv_bridge import CvBridge, CvBridgeError
 import rospy
 from dataclasses import dataclass, field
 from abc import ABC
-from ambf_msgs.msg import CameraState
-from ambf_msgs.msg import RigidBodyState
 import message_filters
 from ambf6dpose.DataCollection.Rostopics import RosTopics, get_topics_processing_cb, topic_to_attr_dict
-
 
 
 @dataclass
@@ -92,77 +82,6 @@ class AbstractSimulationClient(ABC):
         if verbose:
             print(f"Time to get first package data: {last_time - init_time:0.3f}s")
 
-
-@dataclass
-class AMBFClientWrapper(AbstractSimulationClient):
-    sim_manager: SimulationManager = field(
-        default_factory=lambda: SimulationManager("Collect6dpose")
-    )
-
-    def __post_init__(self):
-        super().__post_init__()
-        self.img_subs = ImageSub()
-
-        self.scene = Scene(self.sim_manager)  # Provides access to needle and entry/exit points
-        self.ambf_cam_l = Camera(self.sim_manager, "/ambf/env/cameras/cameraL")
-        self.ambf_cam_frame = ECM(self.sim_manager, "CameraFrame")
-
-        assert self.ambf_cam_l is not None, "CameraL not found"
-        assert self.ambf_cam_frame is not None, "CameraFrame not found"
-
-    def has_data(self) -> bool:
-        return True
-
-    def get_data(self) -> RawSimulationData:
-        needle_pose = pm.toMatrix(self.scene.needle_measured_cp())  # Needle to world
-        camera_l_pose = pm.toMatrix(self.ambf_cam_l.get_T_c_w())  # CamL to CamFrame
-        camera_frame_pose = pm.toMatrix(self.ambf_cam_frame.get_T_c_w())  # CamFrame to world
-        raw_img_l = self.img_subs.left_frame
-        seg_img_l = self.img_subs.seg_left_frame
-
-        self.raw_data = RawSimulationData(
-            camera_l_pose, camera_frame_pose, needle_pose, raw_img_l, seg_img_l
-        )
-
-        return self.raw_data
-
-
-class ImageSub:
-    def __init__(self):
-        self.bridge = CvBridge()
-        self.img_subs = rospy.Subscriber(
-            RosTopics.CAMERA_L_IMAGE.value[0], Image, self.left_callback
-        )
-        self.seg_img_subs = rospy.Subscriber(
-            RosTopics.CAMERA_L_SEG_IMAGE.value[0], Image, self.seg_left_callback
-        )
-        self.left_frame = np.zeros((640, 480, 3), dtype=np.uint8)
-        self.left_ts = None
-        self.seg_left_frame = np.zeros((640, 480, 3), dtype=np.uint8)
-        self.seg_left_ts = None
-
-        # Wait a until subscribers and publishers are ready
-        rospy.sleep(0.5)
-
-    def left_callback(self, msg):
-        try:
-            cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            self.left_frame = cv2_img
-            self.left_ts = msg.header.stamp
-        except CvBridgeError as e:
-            print(e)
-
-    def seg_left_callback(self, msg):
-        try:
-            cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            self.seg_left_frame = cv2_img
-            self.seg_left_ts = msg.header.stamp
-        except CvBridgeError as e:
-            print(e)
-
-
-
-
 @dataclass
 class SyncRosInterface(AbstractSimulationClient):
     def __post_init__(self):
@@ -189,82 +108,130 @@ class SyncRosInterface(AbstractSimulationClient):
             raw_data_dict[topic] = self.callback_dict[topic](input_msg)
 
         self.raw_data = RawSimulationData.from_dict(raw_data_dict)
-        #     if topic == RosTopics.CAMERA_L:
-        #         # camera_l_pose = pm.toMatrix(convert_units(pm.fromMsg(input_msg.pose)))
-        #         camera_l_pose = self.callback_dict[topic](input_msg)
-        #     elif topic == RosTopics.CAMERA_FRAME:
-        #         camera_frame_pose = pm.toMatrix(convert_units(pm.fromMsg(input_msg.pose)))
-        #     elif topic == RosTopics.NEEDLE:
-        #         needle_pose = pm.toMatrix(convert_units(pm.fromMsg(input_msg.pose)))
-        #     elif topic == RosTopics.CAMERA_L_IMAGE:
-        #         camera_l_img = self.bridge.imgmsg_to_cv2(input_msg, "bgr8")
-        #     elif topic == RosTopics.CAMERA_L_SEG_IMAGE:
-        #         camera_l_seg_img = self.bridge.imgmsg_to_cv2(input_msg, "bgr8")
-        #     elif topic == RosTopics.CAMERA_L_DEPTH:
-        #         camera_l_depth = self.callback_dict[topic](input_msg)
 
-        # self.raw_data = RawSimulationData(
-        #     camera_l_pose=camera_l_pose,
-        #     camera_frame_pose=camera_frame_pose,
-        #     needle_pose=needle_pose,
-        #     camera_l_img=camera_l_img,
-        #     camera_l_seg_img=camera_l_seg_img,
-        #     camera_l_depth=camera_l_depth,
-        # )
+############################################
+### OLD SIMULATION CLIENTS
+############################################
+
+# @dataclass
+# class AMBFClientWrapper(AbstractSimulationClient):
+#     sim_manager: SimulationManager = field(
+#         default_factory=lambda: SimulationManager("Collect6dpose")
+#     )
+
+#     def __post_init__(self):
+#         super().__post_init__()
+#         self.img_subs = ImageSub()
+
+#         self.scene = Scene(self.sim_manager)  # Provides access to needle and entry/exit points
+#         self.ambf_cam_l = Camera(self.sim_manager, "/ambf/env/cameras/cameraL")
+#         self.ambf_cam_frame = ECM(self.sim_manager, "CameraFrame")
+
+#         assert self.ambf_cam_l is not None, "CameraL not found"
+#         assert self.ambf_cam_frame is not None, "CameraFrame not found"
+
+#     def has_data(self) -> bool:
+#         return True
+
+#     def get_data(self) -> RawSimulationData:
+#         needle_pose = pm.toMatrix(self.scene.needle_measured_cp())  # Needle to world
+#         camera_l_pose = pm.toMatrix(self.ambf_cam_l.get_T_c_w())  # CamL to CamFrame
+#         camera_frame_pose = pm.toMatrix(self.ambf_cam_frame.get_T_c_w())  # CamFrame to world
+#         raw_img_l = self.img_subs.left_frame
+#         seg_img_l = self.img_subs.seg_left_frame
+
+#         self.raw_data = RawSimulationData(
+#             camera_l_pose, camera_frame_pose, needle_pose, raw_img_l, seg_img_l
+#         )
+
+#         return self.raw_data
 
 
-@dataclass
-class RosInterface(AbstractSimulationClient):
-    def __post_init__(self):
-        super().__post_init__()
-        self.bridge = CvBridge()
-        self.camera_l_subs = rospy.Subscriber(
-            RosTopics.CAMERA_L.value[0], CameraState, self.camera_l_callback
-        )
-        self.camera_l_img_subs = rospy.Subscriber(
-            RosTopics.CAMERA_L_IMAGE.value[0], Image, self.camera_l_img_callback
-        )
-        self.camera_l_seg_img_subs = rospy.Subscriber(
-            RosTopics.CAMERA_L_SEG_IMAGE.value[0], Image, self.camera_l_seg_img_callback
-        )
-        self.camera_frame_subs = rospy.Subscriber(
-            RosTopics.CAMERA_FRAME.value[0], RigidBodyState, self.camera_frame_callback
-        )
-        self.needle_subs = rospy.Subscriber(
-            RosTopics.NEEDLE.value[0], RigidBodyState, self.needle_callback
-        )
+# class ImageSub:
+#     def __init__(self):
+#         self.bridge = CvBridge()
+#         self.img_subs = rospy.Subscriber(
+#             RosTopics.CAMERA_L_IMAGE.value[0], Image, self.left_callback
+#         )
+#         self.seg_img_subs = rospy.Subscriber(
+#             RosTopics.CAMERA_L_SEG_IMAGE.value[0], Image, self.seg_left_callback
+#         )
+#         self.left_frame = np.zeros((640, 480, 3), dtype=np.uint8)
+#         self.left_ts = None
+#         self.seg_left_frame = np.zeros((640, 480, 3), dtype=np.uint8)
+#         self.seg_left_ts = None
 
-        time.sleep(0.2)
+#         # Wait a until subscribers and publishers are ready
+#         rospy.sleep(0.5)
 
-    def camera_l_callback(self, msg):
-        self.camera_l_pose = pm.toMatrix(convert_units(pm.fromMsg(msg.pose)))
+#     def left_callback(self, msg):
+#         try:
+#             cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+#             self.left_frame = cv2_img
+#             self.left_ts = msg.header.stamp
+#         except CvBridgeError as e:
+#             print(e)
 
-    def camera_l_img_callback(self, msg):
-        self.camera_l_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+#     def seg_left_callback(self, msg):
+#         try:
+#             cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+#             self.seg_left_frame = cv2_img
+#             self.seg_left_ts = msg.header.stamp
+#         except CvBridgeError as e:
+#             print(e)
 
-    def camera_l_seg_img_callback(self, msg):
-        self.camera_l_seg_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+# @dataclass
+# class RosInterface(AbstractSimulationClient):
+#     def __post_init__(self):
+#         super().__post_init__()
+#         self.bridge = CvBridge()
+#         self.camera_l_subs = rospy.Subscriber(
+#             RosTopics.CAMERA_L.value[0], CameraState, self.camera_l_callback
+#         )
+#         self.camera_l_img_subs = rospy.Subscriber(
+#             RosTopics.CAMERA_L_IMAGE.value[0], Image, self.camera_l_img_callback
+#         )
+#         self.camera_l_seg_img_subs = rospy.Subscriber(
+#             RosTopics.CAMERA_L_SEG_IMAGE.value[0], Image, self.camera_l_seg_img_callback
+#         )
+#         self.camera_frame_subs = rospy.Subscriber(
+#             RosTopics.CAMERA_FRAME.value[0], RigidBodyState, self.camera_frame_callback
+#         )
+#         self.needle_subs = rospy.Subscriber(
+#             RosTopics.NEEDLE.value[0], RigidBodyState, self.needle_callback
+#         )
 
-    def camera_frame_callback(self, msg):
-        self.camera_frame_pose = pm.toMatrix(convert_units(pm.fromMsg(msg.pose)))
+#         time.sleep(0.2)
 
-    def needle_callback(self, msg):
-        self.needle_pose = pm.toMatrix(convert_units(pm.fromMsg(msg.pose)))
+#     def camera_l_callback(self, msg):
+#         self.camera_l_pose = pm.toMatrix(convert_units(pm.fromMsg(msg.pose)))
 
-    def has_data(self) -> bool:
-        return (
-            self.camera_l_pose is not None
-            and self.camera_frame_pose is not None
-            and self.needle_pose is not None
-        )
+#     def camera_l_img_callback(self, msg):
+#         self.camera_l_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-    def get_data(self) -> RawSimulationData:
-        self.raw_data = RawSimulationData(
-            camera_l_pose=self.camera_l_pose,
-            camera_frame_pose=self.camera_frame_pose,
-            needle_pose=self.needle_pose,
-            camera_l_img=self.camera_l_img,
-            camera_l_seg_img=self.camera_l_seg_img,
-        )
+#     def camera_l_seg_img_callback(self, msg):
+#         self.camera_l_seg_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-        return self.raw_data
+#     def camera_frame_callback(self, msg):
+#         self.camera_frame_pose = pm.toMatrix(convert_units(pm.fromMsg(msg.pose)))
+
+#     def needle_callback(self, msg):
+#         self.needle_pose = pm.toMatrix(convert_units(pm.fromMsg(msg.pose)))
+
+#     def has_data(self) -> bool:
+#         return (
+#             self.camera_l_pose is not None
+#             and self.camera_frame_pose is not None
+#             and self.needle_pose is not None
+#         )
+
+#     def get_data(self) -> RawSimulationData:
+#         self.raw_data = RawSimulationData(
+#             camera_l_pose=self.camera_l_pose,
+#             camera_frame_pose=self.camera_frame_pose,
+#             needle_pose=self.needle_pose,
+#             camera_l_img=self.camera_l_img,
+#             camera_l_seg_img=self.camera_l_seg_img,
+#         )
+
+#         return self.raw_data
