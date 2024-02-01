98dcbc3b457d74407673dee0874410ead29f97ee
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-02-01 13:29:08 -0500

    Clean simple_replay.py

--- scripts/simple_replay.py
+++ scripts/simple_replay.py
@@ -9,7 +9,7 @@ from click_params import FloatListParamType
 import rospy
 import cv2
 
-def_path = "/home/juan1995/research_juan/accelnet_grant/6d_pose_dataset_collection/test_replay/src_env2_v1.1.3_rec1_jack.bag"
+default_path = "/home/juan1995/research_juan/accelnet_grant/6d_pose_dataset_collection/test_replay/src_env2_v1.1.3_rec03_jack.bag"
 
 
 @dataclass
@@ -64,7 +64,12 @@ def setup_sigint_handler(bag_player: rosbagutils.RosbagReplayer):
 
 
 @click.command()
-@click.option("--bag_path", default=def_path, help="Path to bag file", type=click.Path(exists=True))
+@click.option(
+    "--bag_path",
+    default=default_path,
+    help="Path to bag file",
+    type=click.Path(exists=True),
+)
 @click.option("--percent_to_replay", default=1.0, help="Path to bag file")
 @click.option(
     "--ecm_pos",
@@ -80,7 +85,7 @@ def setup_sigint_handler(bag_player: rosbagutils.RosbagReplayer):
     type=click.Path(file_okay=False, dir_okay=True, path_type=Path),
     help="Only required if record flag (-r) is set.",
 )
-def replay_motions(bag_path, percent_to_replay, ecm_pos, record_im, output_p: Path):
+def single_replay(bag_path, percent_to_replay, ecm_pos, record_im, output_p: Path):
     assert len(ecm_pos) == 4 or len(ecm_pos) == 0, "ecm jp needs 4 values"
 
     if record_im:
@@ -104,7 +109,11 @@ def replay_motions(bag_path, percent_to_replay, ecm_pos, record_im, output_p: Pa
     if record_im:
         with img_recorder:
             bag_player.run_replay(
-                psm1_pos, psm1_jaw, psm2_pos, psm2_jaw, percent_to_replay=percent_to_replay
+                psm1_pos,
+                psm1_jaw,
+                psm2_pos,
+                psm2_jaw,
+                percent_to_replay=percent_to_replay,
             )
     else:
         bag_player.run_replay(
@@ -117,9 +126,13 @@ def replay_motions(bag_path, percent_to_replay, ecm_pos, record_im, output_p: Pa
 
 
 @click.command()
-@click.option("--bag_path", default=def_path, help="Path to bag file")
+@click.option("--bag_path", default=default_path, help="Path to bag file")
+@click.option("--n_replays", default=2, help="Path to bag file")
 @click.option("--percent_to_replay", default=1.0, help="Path to bag file")
-def loop_replay(bag_path, percent_to_replay):
+def loop_replay(bag_path, n_replays, percent_to_replay):
+    """
+    Replay motions from bag file N times.
+    """
     # Init ambf objects
     bag_player = rosbagutils.RosbagReplayer()
 
@@ -129,11 +142,15 @@ def loop_replay(bag_path, percent_to_replay):
     ecm_pos, psm1_pos, psm2_pos, psm1_jaw, psm2_jaw = rosbagutils.read_rosbag(bag_path)
 
     try:
-        for i in range(6):
+        for i in range(n_replays):
             print(f"Replay {i}")
             bag_player.move_psm_to_start(psm1_pos[0], psm2_pos[0])
             bag_player.run_replay(
-                psm1_pos, psm1_jaw, psm2_pos, psm2_jaw, percent_to_replay=percent_to_replay
+                psm1_pos,
+                psm1_jaw,
+                psm2_pos,
+                psm2_jaw,
+                percent_to_replay=percent_to_replay,
             )
             time.sleep(0.3)
 
@@ -149,6 +166,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main.add_command(replay_motions)
+    main.add_command(single_replay)
     main.add_command(loop_replay)
     main()
