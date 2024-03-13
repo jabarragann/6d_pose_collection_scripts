c52b03e4f787a7a8c5b6fa1f4818f6c5c9befc28
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-03-13 15:36:46 -0400

    Move camera to generate multi view images with new  data generation script

--- scripts/scene_randomization/gen_data_with_LND.py
+++ scripts/scene_randomization/gen_data_with_LND.py
@@ -6,6 +6,8 @@ from typing import List
 
 from click import Tuple
 import click
+from click_params import IntListParamType
+import numpy as np
 from ambf6dpose.DataCollection.BOPSaver.BopSaver import BopSampleSaver
 from ambf6dpose.DataCollection.RosClients import SyncRosInterface
 from ambf6dpose.DataCollection.SimulatorDataProcessor import SimulatorDataProcessor
@@ -13,6 +15,41 @@ from ambf6dpose.SimulationObjs.psm_lnd.PSM_LND import LND
 from dataclasses import dataclass, field
 from ambf_client import Client
 from PyKDL import Frame
+from surgical_robotics_challenge.ecm_arm import ECM
+from surgical_robotics_challenge.simulation_manager import SimulationManager
+
+
+def get_camera_positions() -> List[List[float]]:
+    ecm_list = []
+    ecm_list.append([0.0, 0.0, 0.0, 0.0])  # 0
+    ecm_list.append([0.0, 0.05, -0.00, 0.0])  # 1
+    ecm_list.append([0.0, 0.05, -0.00, 0.4])  # 2
+    ecm_list.append([0.0, 0.05, -0.00, -0.4])  # 3
+    ecm_list.append([0.2, 0.05, -0.00, 0.0])  # 4
+    ecm_list.append([-0.2, 0.05, -0.00, 0.0])  # 5
+    ecm_list.append([0.0, 0.15, -0.00, 0.0])  # 6
+    ecm_list.append([0.0, -0.05, -0.00, 0.0])  # 7
+    ecm_list.append([0.0, 0.05, -0.005, 0.0])  # 8
+    ecm_list.append([0.0, 0.05, -0.005, 0.0])  # 9
+    ecm_list.append([0.1, 0.05, -0.005, 0.2])  # 10
+    ecm_list.append([-0.1, 0.05, -0.005, -0.2])  # 11
+    ecm_list.append([0.1, 0.10, 0.001, 0.0])  # 12
+    ecm_list.append([-0.1, 0.0, 0.001, 0.0])  # 13
+    ecm_list.append([0.0, 0.10, -0.005, 0.3])  # 14
+    ecm_list.append([0.0, 0.0, 0.001, 0.3])  # 15
+    ecm_list.append([0.1, 0.10, -0.006, 0.1])  # 16
+    ecm_list.append([-0.1, 0.0, -0.006, -0.1])  # 17
+    ecm_list.append([0.1, 0.10, -0.003, -0.1])  # 18
+    ecm_list.append([-0.1, 0.0, 0.00, 0.1])  # 19
+
+    # Add random noise to ECM joint positions
+    for i in range(len(ecm_list)):
+        ecm_list[i][0] += 0.06 * (2 * np.random.rand() - 1)
+        ecm_list[i][1] += 0.06 * (2 * np.random.rand() - 1)
+        ecm_list[i][2] += 0.003 * (2 * np.random.rand() - 1)
+        ecm_list[i][3] += 0.08 * (2 * np.random.rand() - 1)
+
+    return ecm_list
 
 
 @dataclass
@@ -113,6 +150,17 @@ class InstrumentPoseRandomizer:
         return random.uniform(0.0, 0.7)
 
 
+class DummySaver:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+    def save_sample(self, *args):
+        pass
+
+
 def init_client() -> Client:
     client = Client("LND_Simulation")
     client.connect()
@@ -125,41 +173,6 @@ def init_client() -> Client:
     return client
 
 
-@click.command()
-@click.option("--total_steps", default=3, help="Total number of steps")
-@click.option("--step_size", default=0.017, help="Step size")
-def replay(total_steps, step_size):
-
-    client = init_client()
-
-    psm1_lnd = LND("/new_psm1/", client)
-    psm2_lnd = LND("/new_psm2/", client)
-
-    initial_rpy_psm1 = psm1_lnd.base.get_pose()[3:]
-    initial_rpy_psm2 = psm2_lnd.base.get_pose()[3:]
-
-    psm1_trajectory = PSM1Traj(
-        initial_rpy_psm1, total_steps=total_steps, step_size=step_size
-    )
-    psm2_trajectory = PSM2Traj(
-        initial_rpy_psm2, total_steps=total_steps, step_size=step_size
-    )
-
-    psm1_randomizer = InstrumentPoseRandomizer(psm1_lnd)
-    psm2_randomizer = InstrumentPoseRandomizer(psm2_lnd)
-
-    for t1, t2 in zip(psm1_trajectory, psm2_trajectory):
-        psm1_randomizer.update(t1)
-        psm2_randomizer.update(t2)
-
-        time.sleep(1.4)
-
-    psm1_randomizer.reset()
-    psm2_randomizer.reset()
-
-    time.sleep(0.5)
-
-
 def wait_for_data(client: SyncRosInterface):
     try:
         client.wait_for_data(12)
@@ -170,58 +183,118 @@ def wait_for_data(client: SyncRosInterface):
         )
         sys.exit(1)
 
+def extract_wanted_ecm_positions(wanted_positions:List[int]) -> List[List[float]]:
 
-@click.command()
+    ecm_full_list = get_camera_positions()
+
+    if len(wanted_positions) == 0:
+        wanted_positions = list(range(0, len(ecm_full_list)))
+
+    assert all([i >= 0 for i in wanted_positions]), "ECM positions must be positive integers"
+    assert all(
+        [i < len(ecm_full_list) for i in wanted_positions]
+    ), f"Only {len(ecm_full_list)} ECM positions are available"
+
+    ecm_jp_list = [ecm_full_list[i] for i in wanted_positions]
+
+    return ecm_jp_list
+
+@dataclass
+class ExperimentManager:
+    total_steps: int
+    step_size: float
+
+    def __post_init__(self):
+
+        self.client = init_client()
+        self.sim_manager = SimulationManager("record_test")
+        self.cam_frame_handle = ECM(self.sim_manager, "CameraFrame")
+
+        self.init_psm_randomizers()
+
+    def init_psm_randomizers(self):
+        self.psm1_lnd = LND("/new_psm1/", self.client)
+        self.psm2_lnd = LND("/new_psm2/", self.client)
+
+        initial_rpy_psm1 = self.psm1_lnd.base.get_pose()[3:]
+        initial_rpy_psm2 = self.psm2_lnd.base.get_pose()[3:]
+
+        self.psm1_trajectory = PSM1Traj(
+            initial_rpy_psm1, total_steps=self.total_steps, step_size=self.step_size
+        )
+        self.psm2_trajectory = PSM2Traj(
+            initial_rpy_psm2, total_steps=self.total_steps, step_size=self.step_size
+        )
+
+        self.psm1_randomizer = InstrumentPoseRandomizer(self.psm1_lnd)
+        self.psm2_randomizer = InstrumentPoseRandomizer(self.psm2_lnd)
+
+    def get_psm_trajectories(self):
+        return self.psm1_trajectory, self.psm2_trajectory
+
+    def reset_psm(self):
+        self.psm1_randomizer.reset()
+        self.psm2_randomizer.reset()
+
+
+@click.command("replay-and-record", context_settings={"show_default": True})
 @click.option(
     "--output_dir",
     type=click.Path(file_okay=False, path_type=Path),
-    required=True,
+    default=None,
     help="Output directory",
 )
-@click.option("--scene_id", help="id given to current collection",required=True, type=int)
-@click.option("--total_steps", default=4, help="Total number of steps")
+@click.option("--scene_id", help="id given to current collection", type=int)
+@click.option("--total_steps", default=2, help="Total number of steps")
 @click.option("--step_size", default=0.012, help="Step size")
-def replay_and_record(output_dir, scene_id, total_steps, step_size):
+@click.option("--record", is_flag=True, default=False, help="Record data flag.")
+@click.option(
+    "--ecm_positions",
+    default="",
+    help="Index of ECM positions, specifid as a string of ints separated with a space, \
+    e.g., '1 2 3'. Default value: all available positions",
+    type=IntListParamType(" ", ignore_empty=True),
+)
+def replay_and_record(
+    output_dir, scene_id, total_steps, step_size, record, ecm_positions
+):
 
-    output_dir = Path(output_dir)
-    saver: BopSampleSaver = BopSampleSaver(output_dir, scene_id=scene_id)
+    if record:
+        if output_dir is None:
+            raise ValueError("Output directory must be provided if recording data")
+        if scene_id is None:
+            raise ValueError("Scene id must be provided if recording data")
+
+        output_dir = Path(output_dir)
+        saver: BopSampleSaver = BopSampleSaver(output_dir, scene_id=scene_id)
+    else:
+        saver = DummySaver()
+
     ros_client = SyncRosInterface()
     samples_generator = SimulatorDataProcessor(ros_client)
-
     wait_for_data(ros_client)
 
-    client = init_client()
-    psm1_lnd = LND("/new_psm1/", client)
-    psm2_lnd = LND("/new_psm2/", client)
-
-    initial_rpy_psm1 = psm1_lnd.base.get_pose()[3:]
-    initial_rpy_psm2 = psm2_lnd.base.get_pose()[3:]
-
-    psm1_trajectory = PSM1Traj(
-        initial_rpy_psm1, total_steps=total_steps, step_size=step_size
-    )
-    psm2_trajectory = PSM2Traj(
-        initial_rpy_psm2, total_steps=total_steps, step_size=step_size
-    )
-
-    psm1_randomizer = InstrumentPoseRandomizer(psm1_lnd)
-    psm2_randomizer = InstrumentPoseRandomizer(psm2_lnd)
+    manager = ExperimentManager(total_steps, step_size)
+    camera_positions = extract_wanted_ecm_positions(ecm_positions)
 
     count = 0
     with saver:
-        for t1, t2 in zip(psm1_trajectory, psm2_trajectory):
-            psm1_randomizer.update(t1)
-            psm2_randomizer.update(t2)
+        for camera_pos_idx, cam_jp in enumerate(camera_positions):
+            manager.cam_frame_handle.servo_jp(cam_jp)
+            psm1_traj, psm2_traj = manager.get_psm_trajectories()
 
-            time.sleep(1.4)
-            wait_for_data(samples_generator.simulation_client)
-            data_sample = samples_generator.generate_dataset_sample()
-            saver.save_sample(data_sample)
-            print(f"Collected sample: {count}")
-            count += 1
+            for t1, t2 in zip(psm1_traj, psm2_traj):
+                manager.psm1_randomizer.update(t1)
+                manager.psm2_randomizer.update(t2)
 
-    psm1_randomizer.reset()
-    psm2_randomizer.reset()
+                time.sleep(1.4)
+                wait_for_data(samples_generator.simulation_client)
+                data_sample = samples_generator.generate_dataset_sample()
+                saver.save_sample(data_sample)
+                print(f"Collected sample: {count}")
+                count += 1
+
+    manager.reset_psm()
 
     time.sleep(0.5)
 
@@ -233,5 +306,4 @@ def main():
 
 if __name__ == "__main__":
     main.add_command(replay_and_record)
-    main.add_command(replay)
     main()
